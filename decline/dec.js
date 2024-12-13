903c23052f2f6f6fa6ea6ef7a31493daa8c8d9c1
JaiAnoba <kaeanver@gmail.com>
2024-12-13 08:25:32 -0800

    1st push dash

---
+++ decline/dec.js
@@ -0,0 +1,76 @@
+
+// --- Post Stats Chart ---
+
+
+
+
+
+// --- Activity Chart ---
+
+document.addEventListener('DOMContentLoaded', function () {
+    let activityCanvas = document.getElementById('activityChart').getContext('2d');
+
+    let dpr = window.devicePixelRatio || 1; 
+    activityCanvas.canvas.width = activityCanvas.canvas.clientWidth * dpr;
+    activityCanvas.canvas.height = activityCanvas.canvas.clientHeight * dpr;
+    activityCanvas.scale(dpr, dpr);
+
+    let activityChart = new Chart(activityCanvas, {
+        type: 'line',
+        data: {
+            labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4'], 
+            datasets: [
+                {
+                    label: 'Exhibits Requests', 
+                    data: [24, 25, 40, 50, 45], 
+                    borderColor: '#a20d0d', 
+                    backgroundColor: '#a20d0d', 
+                    fill: false, 
+                    borderWidth: 2
+                },
+                {
+                    label: 'Accepted Exhibits', 
+                    data: [10, 5, 20, 15, 35], 
+                    borderColor: '#ed1c24', 
+                    backgroundColor: '#ed1c24', 
+                    fill: false, 
+                    borderWidth: 2
+                }
+            ]
+        },
+        options: {
+            responsive: true, 
+            maintainAspectRatio: false, 
+            scales: {
+                y: {
+                    beginAtZero: true, 
+                    ticks: {
+                        font: {
+                            size: 11, 
+                            family: 'Poppins', 
+                            weight: 'bold'
+                        }
+                    }
+                },
+                x: {
+                    ticks: {
+                        font: {
+                            size: 14, 
+                            family: 'Poppins', 
+                        }
+                    }
+                }
+            },
+            plugins: {
+                legend: {
+                    labels: {
+                        font: {
+                            size: 14, 
+                            family: 'Poppins', 
+                        }
+                    }
+                }
+            }
+        }
+    });
+});
